23fe861aad4f149bbf03e6f4d4c0e4de3431e9ff
covs67 <43531537+covs67@users.noreply.github.com>
2019-12-29 02:14:16 +0000

    index html changes

---
+++ sketch.js
@@ -0,0 +1,9 @@
+var latVal = document.getElementById('latVal');
+var longVal = document.getElementById('longVal');
+
+latVal.innerHTML = '1.00';
+longVal.innerHTML = '1.00';
+
+function addLocation() {
+    document.getElementById("enterLocation").innerHTML = '<input type="text" value="Enter location...">';
+}
